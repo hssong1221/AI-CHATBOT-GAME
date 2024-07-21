8069dd5ca43ac707c5ec0beade5a6ea832b6e8ff
hssong1221 <hssong1221@naver.com>
2024-07-21 22:15:40 +0900

    유니티 연동

---
+++ CHATGAME/Assets/Assets/Scripts/Manager/LogoManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoManager : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
