6fce9df0dc11a940c49f12882b88350b4ba1cf6c
hssong1221 <hssong1221@naver.com>
2024-07-24 00:08:19 +0900

    엑셀 데이터 연동 완료

---
+++ CHATGAME/Assets/Scripts/Game/UI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI : MonoBehaviour
+{
+    private static UI _instance;
+    public static UI Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject singletonObject = new GameObject();
+                _instance = singletonObject.AddComponent<UI>();
+                singletonObject.name = typeof(UI).ToString() + " (Singleton)";
+                SingletonManager.Instance.RegisterSingleton(_instance);
+            }
+            return _instance;
+        }
+    }
+
+    public Image mainImg;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI affectionText;
+    public TextMeshProUGUI dialogueText;
+
+    DataManager dataManager;
+    SheetData sheet;
+
+    public int Idx = 0;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as UI;
+            SingletonManager.Instance.RegisterSingleton(_instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        dataManager = SingletonManager.Instance.GetSingleton<DataManager>();
+        sheet = dataManager.GetSheetData("Sheet1");
+
+        nameText.text = "name";
+        affectionText.text = "0";
+        dialogueText.text = "dialogue";
+    }
+
+
+    public void SetUI()
+    {
+        var data = sheet.GetData(Idx);
+
+        if (data == null)
+            return;
+
+        if(data.TryGetValue("id", out var id))
+            nameText.text = id;
+
+        if (data.TryGetValue("affection", out var aff))
+            affectionText.text = aff.ToString();
+
+        if (data.TryGetValue("text", out var txt))
+            dialogueText.text = txt;
+
+        Idx++;
+    }
+
+    public void OnClickNextBtn()
+    {
+        SetUI();
+    }
+}
