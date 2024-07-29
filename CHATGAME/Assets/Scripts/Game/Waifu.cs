ef09c2b8b10443725b6e2701165304175247296f
NOPE <kyb0809tg@naver.com>
2024-07-29 13:04:53 +0900

    waifu singleton

--- CHATGAME/Assets/Scripts/Game/Waifu.cs
+++ CHATGAME/Assets/Scripts/Game/Waifu.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class Waifu : MonoBehaviour
 {
+    private static Waifu _Instance;
+
+    public static Waifu Instance
+    {
+        get 
+        {
+            if( _Instance == null )
+            {
+                GameObject singletonObject = new GameObject();
+                _Instance = singletonObject.AddComponent<Waifu>();
+                singletonObject.name = typeof( Waifu ).ToString() + " (Singleton)";
+                SingletonManager.Instance.RegisterSingleton(_Instance);
+            }
+            return _Instance;
+        }
+    }
+
     public static int affection;//singletone 에서 관리할 호감도 수치
     public static string affection_status;//singletone 에서 관리할 호감도 상태
+
+    void Awake()
+    {
+        if( _Instance == null )
+        {
+            _Instance = this as Waifu;
+            SingletonManager.Instance.RegisterSingleton(_Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void affection_ascend()
     {
