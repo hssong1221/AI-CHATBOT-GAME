ce3d62342e29291d0ff75b6ae41ff1b77d046bee
hssong1221 <hssong1221@naver.com>
2024-09-02 21:20:20 +0900

    bgm 추가

---
+++ CHATGAME/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Audio;
+
+    void Start()
+    {
+        SoundPlay();
+    }
+
+    public void SoundPlay()
+    {
+        if(Audio != null && !Audio.isPlaying)
+        {
+            Audio.Play();
+        }
+    }
+
+    
+}
